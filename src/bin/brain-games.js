6434a472eecbfcb32bdfe18f4a3a6dc073bc56a2
sharkstoned <stanlyraschinkin@gmail.com>
2018-02-12 16:04:04 +0300

    setup eslint and fixed errors

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env nodejs
-import {getUserName} from '..';
+import { getUserName } from '..';
 
 console.log('Welcome to the Brain Games!');
 
 const name = getUserName('May I have your name? ');
 console.log(`Hello, ${name}!`);
+
