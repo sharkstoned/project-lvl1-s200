d49096fa5780f7d935cdcc67351053ba3fb13a46
sharkstoned <stanlyraschinkin@gmail.com>
2018-02-12 13:49:01 +0300

    added user name prompt using readline-sync

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env nodejs
+import {getUserName} from '..';
 
 console.log('Welcome to the Brain Games!');
+
+const name = getUserName('May I have your name? ');
+console.log(`Hello, ${name}!`);
